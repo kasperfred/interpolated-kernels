47e57b4820f22f9763e19776f656fff0a319af10
kasperfred <kasper@kasperfred.com>
2019-02-15 06:27:32 +0100

    flutter

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
-from src.layers import InterpolatedConv2d
-from src.utils import symmetric_filters
+# from src.layers import InterpolatedConv2d
+# from src.utils import symmetric_filters
 
-import numpy as np
-import tensorflow as tf
+# import numpy as np
+# import tensorflow as tf
 
 # kernel_positions = np.array([
 #         # h w c
@@ -19,15 +19,59 @@ import tensorflow as tf
 # print (inpt.shape)
 # print (l(inpt))
 
-img = tf.convert_to_tensor(np.ones([1,11,11,1]) , dtype=tf.float32)
-kcs = symmetric_filters(np.array([
-        #h,w,c
-        [0,0],
-        [2,1],
-        [1,1],
-        [3,2]
-    ]), 32)
+# img = tf.convert_to_tensor(np.ones([1,11,11,1]) , dtype=tf.float32)
+# kcs = symmetric_filters(np.array([
+#         #h,w,c
+#         [0,0],
+#         [2,1],
+#         [1,1],
+#         [3,2]
+#     ]), 32)
 
-layer = InterpolatedConv2d(kcs, 3)
-v = layer(img)
-print (v)
+# layer = InterpolatedConv2d(kcs, 3)
+# v = layer(img)
+# print (v)
+
+
+import os
+import pickle
+
+run_name = "test"
+
+
+# def save_run(res_dict: dict, path: str=None):
+#     if not path:
+#         fname = res_dict["name"] + ".res.pkl"
+
+#         directory = os.path.join("results", run_name)
+
+#         # create directories
+#         if not os.path.exists(directory):
+#             os.makedirs(directory)
+
+#         path = os.path.join(directory, fname)
+
+#     # save data
+#     with open(path, 'wb') as f:
+#         pickle.dump(res_dict, f)
+
+#     return 0
+
+
+# save_run({"name": "hi"})
+
+
+import tensorflow as tf
+import numpy as np
+
+a = np.array([
+    [1,2,3,4],
+    [1,2,3,4],
+    [1,2,3,4]
+])
+
+a = tf.convert_to_tensor(a)
+
+print (a)
+
+print (np.array(a))
