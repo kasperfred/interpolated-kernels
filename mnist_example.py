8ed3a33c71e564a89105b6eea866a334ce2f8666
kasperfred <kasper@kasperfred.com>
2019-02-15 06:25:24 +0100

    fix upsampling memory error

--- mnist_example.py
+++ mnist_example.py
@@ -11,6 +11,7 @@ import numpy as np
 from timer import Timer
 import os
 import pickle
+import cv2
 
 # debug
 verbose = True
@@ -37,27 +38,42 @@ input_shape = (im_size, im_size, 1)
 run_name = f"run-i{im_size}-k{kernel_size}-v{run_version}"
 
 
-def get_data(size=28):
+def get_data(size=28, resize_method="cv2"):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
     x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
+    x_train = x_train / 255
+    x_test = x_test / 255
+    
     if not size == 28:
-        x_train = tf.image.resize_images(
-            x_train,
-            (size, size),
-            method=tf.image.ResizeMethod.BILINEAR,
-        )
+        if resize_method == "tf":
+            x_train = tf.image.resize_images(
+                x_train,
+                (size, size),
+                method=tf.image.ResizeMethod.BILINEAR,
+            )
 
-        x_test = tf.image.resize_images(
-            x_test,
-            (size, size),
-            method=tf.image.ResizeMethod.BILINEAR,
-        )    
+            x_test = tf.image.resize_images(
+                x_test,
+                (size, size),
+                method=tf.image.ResizeMethod.BILINEAR,
+            )
+        elif resize_method == "cv2":
+            x_train = np.array([
+                cv2.resize(x, (size,size))
+                for x in x_train])
+            
+            x_test = np.array([
+                cv2.resize(x, (size,size))
+                for x in x_test])
+        else: 
+            raise ValueError("Invalid resize method. Use 'cv2' or 'tf'.")
 
-    x_train /= 255
-    x_test /= 255
+
+    x_train = x_train.reshape(x_train.shape[0], size, size, 1)
+    x_test = x_test.reshape(x_test.shape[0], size, size, 1)
 
     # convert class vectors to binary class matrices
     y_train = keras.utils.to_categorical(y_train, 10)
@@ -68,10 +84,34 @@ def get_data(size=28):
 
 (x_train, y_train), (x_test, y_test) = get_data(im_size)
 
+def datagen(batch_size, x, y, method="cycle"):
+    if method == "random":
+        while True:
+            # choose random batch sample
+            idx = np.random.randint(0, x_train.shape[0], batch_size)
+            x_batch = x[idx]
+            y_batch = y[idx]
+            yield (x_batch, y_batch)
+    elif method == "cycle":
+        while True:
+            n_samples = int(x_train.shape[0])
+
+            for i in range(int(n_samples/batch_size)):
+                batch_start = i * batch_size
+                batch_end = (i+1) * batch_size
+
+                x_batch = x[batch_start:batch_end]
+                y_batch = y[batch_start:batch_end]
+
+                yield (x_batch, y_batch)
+    else:
+        raise ValueError("Method is not supported. Use 'random' or 'cycle'.")
+
 if verbose:
     print('x_train shape:', x_train.shape)
     print(x_train.shape[0], 'train samples')
     print(x_test.shape[0], 'test samples')
+
 
 def build_baseline(input_shape, kernel_size, compile=True, *args, **kwargs):
     model = Sequential()
@@ -148,12 +188,14 @@ def run_model(model_build_function, name=None, verbose=True):
     model = build_baseline(input_shape, kernel_size=kernel_size,
                            kernel_positions=kernel_positions, compile=True)
 
+    verbose_fixed = 1 if verbose else 0
+
     t = Timer().start()
     hist = model.fit(x_train, y_train,
-                     batch_size=batch_size,
-                     epochs=epochs,
-                     verbose=1,
-                     validation_data=(x_test, y_test))
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=verbose_fixed,
+                    validation_data=(x_test, y_test))
     t.stop()
 
     score = model.evaluate(x_test, y_test, verbose=0)
@@ -192,6 +234,8 @@ def save_run(res_dict: dict, path: str=None):
         pickle.dump(res_dict, f)
 
     return 0
+
+
 
 
 # run models
